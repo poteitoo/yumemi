645b8176c67486c9077dc2f0c61343fef1038724
poteitoo <1105tigery@gmail.com>
2024-01-13 15:53:43 +0900

    split files

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Link from "next/link";
+"use client";
+
+import { HomePage } from "@/views/home/page";
 
 export default function Page() {
-  return (
-    <div>
-      <h1>Home</h1>
-      <Link href="/about">About</Link>
-    </div>
-  );
+  return <HomePage />;
 }

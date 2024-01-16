b51bb83875c0de04c403e44fd1ef311c3e7ee4f5
poteitoo <1105tigery@gmail.com>
2024-01-16 21:48:49 +0900

    add page styles

--- views/home/page.tsx
+++ views/home/page.tsx
@@ -2,6 +2,7 @@ import { PrefectureCheckList } from "./prefecutre-check-list";
 import { useHomePage } from "./hooks";
 import { PopulationChart } from "./population-chart";
 import { CategoryRadioList } from "./category-radio-list";
+import style from "./styles.module.css";
 
 export function HomePage() {
   const {
@@ -14,16 +15,20 @@ export function HomePage() {
   } = useHomePage();
 
   return (
-    <div style={{ height: "50vh", width: "50%" }}>
+    <div className={style.container}>
+      <h1>人口構成チャート</h1>
+      <h2>都道府県</h2>
       <PrefectureCheckList
         prefectures={prefectures}
         checkedPrefCodes={checkedPrefCodes}
         onCheckChange={handleCheckedPrefectureChange}
       />
+      <h2>カテゴリー</h2>
       <CategoryRadioList
         category={checkedCategory}
         onCategoryChange={handleCategoryChange}
       />
+      <h2>チャート</h2>
       <PopulationChart
         population={populationCompositions}
         category={checkedCategory}
